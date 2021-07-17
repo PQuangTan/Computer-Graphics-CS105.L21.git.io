2bb0cc90dedb77c9ace4da66d16e44e5fb493449
PQuangTan <55052788+PQuangTan@users.noreply.github.com>
2021-07-17 17:17:28 +0700

    Upload texture

--- examples/js/main.js
+++ examples/js/main.js
@@ -4,7 +4,7 @@ import { TeapotGeometry } from '../jsm/geometries/TeapotGeometry.js';
 import { GUI } from '../jsm/libs/dat.gui.module.js';
 import { TransformControls } from '../jsm/controls/TransformControls.js';
 
-let camera, scene, renderer, gui, control, orbit, mesh, geometry, material, light, texture, plane;
+let camera, scene, renderer, gui, control, orbit, mesh, geometry, material, light, texture, plane, image_path;
 let size = 30;
 var data = {
     "model": "Box",
@@ -38,8 +38,9 @@ function init() {
     scene = new THREE.Scene();
 	plane = getPlane(10000);
 	plane.rotation.x = -Math.PI/2;
+    plane.position.y = -15;
 	scene.add(plane);
-    scene.add(new THREE.GridHelper(10000, 100, 0x888888, 0x444444));
+    // scene.add(new THREE.GridHelper(10000, 100, 0x888888, 0x444444));
 
 	// Light
     light = new THREE.PointLight(0xffffff, 3);
@@ -49,8 +50,31 @@ function init() {
     scene.add(light);
 
 	// Load Texture
-    texture = new THREE.TextureLoader().load('textures/scratch.jpg', render);
-    texture.anisotropy = renderer.capabilities.getMaxAnisotropy();
+    var GuiConfig = function() {
+        this['Image Path'] = '';  // default image path
+        var file;
+        this['Upload Image'] = function() {
+            // you need to create an input element in HTML, explained later
+    
+            var input = document.getElementById('img-path');
+            input.addEventListener('change', function() {
+                file = input.files[0];
+                // image_path = input.value;
+                config['Image Path'] = file.name;
+                //image_path = file.name;
+                image_path = URL.createObjectURL( file )
+                // update all controllers
+                var path = (window.URL || window.webkitURL).createObjectURL(file);
+                console.log('path', path);
+    
+                for (var i in gui.__controllers) {
+                    gui.__controllers[i].updateDisplay();
+                }
+            });
+            input.click();
+        };
+    };
+    var config = new GuiConfig();
 
 	// Mesh
     geometry = new THREE.BoxGeometry(size, size, size);
@@ -85,6 +109,7 @@ function init() {
     objectFolder.add(data, 'transform', ['None', 'Translate', 'Scale', 'Rotate']).name('Transform Mode').onChange(transformMode);
     objectFolder.add(data, 'model', ["Box", "Sphere", "Cone", "Cylinder", "Torus", "Knot", "Teapot", "Tetrahedron", "Octahedron", "Dodecahedron", "Icosahedron"]).name('Model').onChange(generateGeometry);
     objectFolder.add(data, 'surface', ["Solid", "Wireframes", "Points", "Texture"]).name('Surface').onChange(generateGeometry);
+    objectFolder.add(config, 'Upload Image');
     objectFolder.addColor( data, 'objectcolor' ).name('Color').onChange(function(value) {mesh.material.color.set(value)});
     
     let cameraFolder = gui.addFolder('Camera');
@@ -168,6 +193,8 @@ function generateGeometry() {
             size: 1
         }));
     } else if (data.surface == "Texture") {
+        texture = new THREE.TextureLoader().load(image_path, render);
+        texture.anisotropy = renderer.capabilities.getMaxAnisotropy();
         mesh = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({
             color: 'rgb(255, 255, 255)',
             map: data.surface === "Texture" ? texture : null
@@ -177,6 +204,7 @@ function generateGeometry() {
             color: 'rgb(255, 255, 255)'
         }));
     }
+    console.log(mesh.show)
     mesh.castShadow = true;
 	scene.add(mesh);
     transformMode();
